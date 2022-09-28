801cf593a2650b7199683975232b9e635161112b
4elovekD <slava04.06.98@mail.ru>
2022-09-28 14:50:05 +0300

    HW-3

---
+++ lesson-3/index.js
@@ -0,0 +1,80 @@
+/* //1
+for (i=1; i<=50; i++){
+    console.log(i);
+}
+
+for (i=35; i>=8; i--){
+    console.log(i);
+} */
+
+
+
+/* //2
+let i = 90;
+while (i>11){
+    i--;
+    console.log(i)
+} */
+
+
+
+
+/* //3
+let a = 100;
+s = 0;
+for (i= 0; i<= a; i++){
+    s= s+i;
+    console.log(s);
+} */
+
+
+
+/* //4
+let a = 3;
+s = 0;
+for (i= 0; i<= a; i++){
+    s= s+i;
+}
+
+console.log(s); */
+
+
+
+/* //5
+for(i=8; i<=56; i++){
+    if(i%2===0){
+        console.log(i);
+    }
+} */
+
+
+
+
+/* //6
+for(i = 2; i<=10; i++){
+    for(a = 2; a<=9; a++){
+        document.write(i + "*" + a + "=" +(i*a) + "<br>");
+        
+    }
+    document.write('<br>');
+} */
+
+/* //7
+let n =1000,
+    num = 0;
+ for(; n>50; n=n/2){
+    num++;
+    if (n<50){
+        break;
+        
+    }
+ }
+ console.log('Ответ: ' + n);
+ console.log('Количество итераций: ' + num); */
+
+//8
+
+//9
+
+
+//10
