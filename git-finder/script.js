2d90f0cd040ebfd6f07ca58450ba5831e854350c
4elovekD <slava04.06.98@mail.ru>
2022-10-28 12:03:26 +0300

    fix

--- git-finder/script.js
+++ git-finder/script.js
@@ -2,39 +2,43 @@ const user = document.querySelector('.input')
 const userInfo = document.querySelector('.user-info')
 
 
-user.addEventListener('keyup', searchUser);
-user.addEventListener('keydown', clearPage);
-
-
-async function searchUser(){
+user.addEventListener('keyup', function(){
     let userName = user.value
-    const url = fetch(`https://api.github.com/users/${userName}`)
-    const response = await(url)
-    const data = await response.json()
-    console.log(data)
-    if (data.message != 'Not found'){
-        let HTML = `        
-               <img src="${data.avatar_url}">
-                <ul>
-                    <li>bio: ${data.bio}</li>
-                    <li>company: ${data.company}</li>
-                    <li>created at: ${data.created_at}</li>
-                    <li>followers: ${data.followers}</li>
-                    <li>following: ${data.folowing}</li>
-                </ul>`
-        userInfo.insertAdjacentHTML("beforeend", HTML)
-        console.log(userInfo)
-        return userInfo
-    } return
+    if(userName != ''){
+        fetch(`https://api.github.com/users/${userName}`).then((res)=>{
+            console.log(res)
+            return res.json()
+        }).then((res)=>{
+            createUser(res)
+        })
+    }else(
+        clear()
+    )
+});
+
+function createUser(response){
+    console.log(response)
+    userInfo.style.display = 'flex';
+
+    if(response.name !== undefined){
+        userInfo.innerHTML = `
+        <div class="user-img__wrapper">
+                <img class="user-img" src="${response.avatar_url}" />
+            </div>
+            <div class="user-info__wrapper">
+                <div class="user-info__text">Name: <span>${response.name}</span></div>
+                <div class="user-info__text">BIO: <span>${response.bio}</span></div>
+                <div class="user-info__text">Location: <span>${response.location}</span></div>
+                <div class="user-info__text">Company: <span>${response.company}</span></div> 
+                <div class="user-info__text">ID: <span>${response.id}</span></div> 
+            </div>
+        `
+    }
 }
 
-
-function clearPage(){
-    userInfo.innerHTML = '';
+function clear(){
+    userInfo.style.display = 'none'
 }
-
-
-
 
 
 
