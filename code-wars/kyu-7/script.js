16a2692efdc84bc250e6de7284325e6b14729d34
4elovekD <slava04.06.98@mail.ru>
2022-10-31 15:43:05 +0300

    done 20 answers

--- code-wars/kyu-7/script.js
+++ code-wars/kyu-7/script.js
@@ -1,4 +1,3 @@
-
 //1
 /* function squareSum(numbers){
     let sum = 0;
@@ -98,8 +97,8 @@
     return arr2
   } */
 
-  //10
-  //https://www.codewars.com/kata/5813d19765d81c592200001a/train/javascript
+//10
+//https://www.codewars.com/kata/5813d19765d81c592200001a/train/javascript
 
 /* function dontGiveMeFive(start, end) {
   let arr1 = [];
@@ -107,3 +106,114 @@
     if (String(i).indexOf(5) != -1) continue
     else arr1.push(i);
   }  return arr1.length} */
+
+//11 https://www.codewars.com/kata/563cf89eb4747c5fb100001b/train/javascript
+
+/*   function removeSmallest(numbers) {
+    const copy = numbers.slice(0)
+    let smallestValue = numbers.indexOf(Math.min(...numbers))
+    copy.splice(smallestValue, 1);
+    return copy
+   }
+
+console.log(removeSmallest([2,1,1,4,5]));  */
+
+//12 https://www.codewars.com/kata/54bf85e3d5b56c7a05000cf9/train/javascript
+
+/* function number (array){
+  return array.map((item,index) =>{
+    return Number(index+1) + ': ' + item
+  })
+}
+
+console.log(number (["a", "b", "c",'d']));  */
+
+//13 https://www.codewars.com/kata/57cc981a58da9e302a000214/train/javascript
+
+/* function smallEnough(a, limit){
+  let b = ''
+  let arr = a.filter(n =>{if (n <= limit) return true
+  })
+  if(a.length > arr.length) return b =  false
+  else return b = true
+} 
+
+console.log(smallEnough([2,4,5,32,5], 10));  */
+
+//14 https://www.codewars.com/kata/545a4c5a61aa4c6916000755/train/javascript
+
+/* function gimme (triplet) {
+  let arr =[];
+  triplet.forEach(item =>{arr.push(item)})
+  arr.sort((a,b)=> a-b)
+  const n = triplet.findIndex(index =>index == arr[1])
+  return n
+}
+
+gimme ([4,2,3]) */
+
+//15  https://www.codewars.com/kata/5a3dd29055519e23ec000074/train/javascript
+
+/* function checkExam(array1, array2) {
+  let score = 0;
+  array1.forEach((item, index) => {
+    if (array2[index] == "") return
+    if (array1[index] == array2[index]) {
+      return (score += 4);
+    } else return (score += -1);
+  });
+  if (score < 0) return score = 0
+  else return score;
+}
+
+checkExam(["b", "c", "b", "a"], ["",  "a", "a", "c"]) */
+
+//16 https://www.codewars.com/kata/5b16490986b6d336c900007d
+
+/* function myLanguages(results) {
+  const arr = [];
+  const abjArr = Object.entries(results);
+  abjArr.sort((a, b) => b[1] - a[1]);
+  abjArr.forEach(([key, value]) => {
+    if (value < 60) return;
+    else arr.push(key);
+  });
+  return arr;
+}
+
+myLanguages({ Java: 10, Ruby: 80, Python: 65 }); */
+
+
+
+//17 https://www.codewars.com/kata/51f41b98e8f176e70d0002a8/train/javascript
+
+/* sortme = function( names ){
+  return names.sort()
+} */
+
+
+//18 https://www.codewars.com/kata/582746fa14b3892727000c4f/train/javascript
+
+/* function countDevelopers(list) {
+  let a = 0;
+ list.filter(item =>{
+    if (item.continent == "Europe" && item.language == 'JavaScript') return a = a +1
+    else return 
+  })
+  return a
+} */
+
+//19 https://www.codewars.com/kata/55fd2d567d94ac3bc9000064/train/javascript
+
+/* function rowSumOddNumbers(n) {
+	return n**3
+} */
+
+
+//20 https://www.codewars.com/kata/511f11d355fe575d2c000001/train/javascript
+
+/* function twoOldestAges(ages){
+  return ar = ages.sort((a,b)=> a-b)
+                  .slice(-2)
+}
+twoOldestAges([1, 2, 10, 8]) */
